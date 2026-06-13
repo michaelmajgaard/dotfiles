2d1d13f57e682a27548e2de85d08728b8bcf5674
michaelmajgaard <52096428+michaelmajgaard@users.noreply.github.com>
2026-06-13 09:37:41 +0200

    main

---
+++ .config/nvim/init.lua
@@ -0,0 +1,85 @@
+vim.g.mapleader = ';'
+vim.g.maplocalleader = ';'
+
+vim.o.number = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end) 
+
+vim.pack.add({
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/seblyng/roslyn.nvim' },
+
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+})
+
+vim.keymap.set('n', '<leader>ff', function ()
+  require('telescope.builtin').find_files()
+end)
+
+-- Workaround for roslyn.nvim calling removed/internal Nvim API
+vim.lsp.diagnostic = vim.lsp.diagnostic or {}
+vim.lsp.diagnostic._refresh = vim.lsp.diagnostic._refresh or function() end
+
+require('roslyn').setup({
+  config = {
+    root_dir = function(bufnr, on_dir)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+      local root = vim.fs.root(fname, { '*.sln', '*.csproj', '.git' })
+      if root then
+        on_dir(root)
+      end
+    end,
+  },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  end,
+})
+
+
+vim.pack.add({
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/seblyng/roslyn.nvim' },
+})
+
+require('roslyn').setup({
+  config = {
+    root_dir = function(bufnr, on_dir)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+      local root = vim.fs.root(fname, { '*.sln', '*.csproj', '.git' })
+      if root then
+        on_dir(root)
+      end
+    end,
+  },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  end,
+})
+
